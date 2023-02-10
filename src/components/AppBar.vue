0b88fc6e6fd7621e02dd9c34189fcb7fb8e0f39d
Curtis Hamm <curt.hamm@gmail.com>
2023-02-09 23:31:31 -0700

    initial commit

---
+++ src/components/AppBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app-bar color="info">
+    <v-app-bar-title>
+      <v-icon icon="mdi-plane-car" />
+      My Vuetify Project
+    </v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script setup>
+  //
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive
+      class="d-flex align-center text-center fill-height">
+      <v-img contain
+        height="600"
+        src="@/assets/pexels-grafixartphoto-samir-belhamra-4254555.jpg" />
+
+      <div class="text-body-2 font-weight-light mb-n1">
+        Welcome to</div>
+
+      <h1 class="text-h2 font-weight-bold">Vuetify</h1>
+
+      <div class="py-14" />
+
+      <v-row class="d-flex align-center justify-center">
+        <v-col cols="auto">
+        </v-col>
+
+        <v-col cols="auto">
+        </v-col>
+
+        <v-col cols="auto">
+        </v-col>
+      </v-row>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+  //
+</script>
